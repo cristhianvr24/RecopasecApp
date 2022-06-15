fc85c98bae8660bb7ef228dc66ea7c1259167127
unknown <cristhian24vr@gmail.com>
2022-06-15 19:31:54 -0400

    js

--- app/Http/Controllers/ServicioComController.php
+++ app/Http/Controllers/ServicioComController.php
@@ -8,14 +8,24 @@ class ServicioComController extends Controller
 {
     public function index(){
 
-        $estudiantes = Estudiante::paginate();
+        $estudiantes = Estudiante::orderBy('id')->paginate();
         return view('ServicioCom.index', compact('estudiantes'));
     }
     public function create(){
         return view('ServicioCom.create');
     }
     public function store(Request $request){
-        return $request->all();
+        $estudiante = new Estudiante();
+        $estudiante->cedula = $request->cedula;
+        $estudiante->nombre_1 = $request->nombre_1;
+        $estudiante->nombre_2 = $request->nombre_2;
+        $estudiante->apellido_1 = $request->apellido_1;
+        $estudiante->apellido_2 = $request->apellido_2;
+        $estudiante->telefono = $request->telefono;
+        $estudiante->email = $request->email;
+
+        $estudiante->save();
+        return redirect()->route('serviciocom.show', $estudiante);
     }
     public function show($id){
         $estudiante = Estudiante::find($id);
