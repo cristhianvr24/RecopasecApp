1fc6f7bef35cb6fcce0e5bf5687a7c547b4d5638
cristhianvr24 <cristhian24vr@gmail.com>
2022-06-20 11:00:31 -0400

    jetstream

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,14 @@ Route::view('informacionsc', 'ServicioCom.informacion')->name('informacionsc');
 Route::resource('pasantias', PasantiaController::class)->parameters(['pasantias'=>'estudiante']);
 Route::view('informacionp', 'Pasantias.informacion')->name('informacionp');
 
-//bootstrap
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
